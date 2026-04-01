bbcb64c47db64324b17d55bbcf826fd60358792a
Kitrakcodes <kartikcu2006@gmail.com>
2026-04-01 12:37:10 +0530

    first commit

---
+++ JWT-DEMO/src/test/java/com/AML2A/JWT_DEMO/JwtDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.AML2A.JWT_DEMO;
+
+import org.junit.jupiter.api.Disabled;   // 👈 ADD THIS
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled   // 👈 ADD THIS LINE
+class JwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
